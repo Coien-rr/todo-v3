428c36b62dbc5a0cbb89b0b3902a79e24ff92a12
Coienrr <83146518+Coien-rr@users.noreply.github.com>
2021-11-14 16:15:25 +0800

    some change

--- src/store/todo.js
+++ src/store/todo.js
@@ -1,57 +1,64 @@
-import { defineStore } from "pinia";
-import { getTaskList, addTodoTask, updateTodoTask, callCleanAllTaskAPI } from "../utils/todolist";
+import { defineStore } from 'pinia';
+import {
+	getTaskList,
+	addTodoTask,
+	updateTodoTask,
+	callCleanAllTaskAPI,
+} from '../utils/todolist';
+
 export const todoStore = defineStore('todos', {
-  state: ()=>({
-    todos: [],
-    filter: 'all',
-    nextId: 0
-  }),
-  getters: {
-    finishedTodos(state) {
-      return state.todos.filter((todo) => todo.isFinished);
-    },
-    unfinishedTodos(state) {
-      return state.todos.filter((todo) => !todo.isFinished);
-    },
-    filteredTodos(state) {
-      if (this.filter === 'finished') {
-        return this.finishedTodos;
-      } else if (this.filter === 'unfinished') {
-        return this.unfinishedTodos;
-      }
-      return this.todos;
-    },
-  },
-  actions: {
-    initialTodoList(){
-      this.todos = [];
-      const heoo = 'Hello! Server!';
-      getTaskList(heoo).then( res => {
-        console.log(res.data.todolist);
-        let tasklist = res.data.todolist;
-        for(let i = 0; i < tasklist.length; ++i){
-          this.todos.push(tasklist[i]);
-        }
-        this.nextId = tasklist.length + 1;
-        console.log(this.nextId);
-      })
-    },
+	state: () => ({
+		todos: [],
+		filter: 'all',
+		nextId: 0,
+	}),
 
-    addTodoTask(text){
-      this.todos.push({text, id: this.nextId++, isFinished: false});
-      addTodoTask(this.todos[this.nextId-2]);
-    },
+	getters: {
+		finishedTodos(state) {
+			return state.todos.filter((todo) => todo.isFinished);
+		},
+		unfinishedTodos(state) {
+			return state.todos.filter((todo) => !todo.isFinished);
+		},
+		filteredTodos(state) {
+			if (this.filter === 'finished') {
+				return this.finishedTodos;
+			} else if (this.filter === 'unfinished') {
+				return this.unfinishedTodos;
+			}
+			return this.todos;
+		},
+	},
 
-    changeTodoTask(todoId){
-      console.log('call!');
-      updateTodoTask(todoId).then(res => console.log(res));
-    },
+	actions: {
+		initialTodoList() {
+			this.todos = [];
+			const heoo = 'Hello! Server!';
+			getTaskList(heoo).then((res) => {
+				console.log(res.data.todolist);
+				let tasklist = res.data.todolist;
+				for (let i = 0; i < tasklist.length; ++i) {
+					this.todos.push(tasklist[i]);
+				}
+				this.nextId = tasklist.length + 1;
+				console.log(this.nextId);
+			});
+		},
 
-    cleanAllTask(){
-      console.log("callCleanAllTaskAPI!");
-      callCleanAllTaskAPI().then( res => console.log(res.data.result))
-      this.initialTodoList();
-    }
+		addTodoTask(text) {
+			this.todos.push({ text, id: this.nextId++, isFinished: false });
+			addTodoTask(this.todos[this.nextId - 2]);
+		},
 
-  }
-})
+		changeTodoTask(todoId) {
+			console.log('call!');
+			updateTodoTask(todoId).then((res) => console.log(res));
+		},
+
+		cleanAllTask() {
+			console.log('callCleanAllTaskAPI!');
+			callCleanAllTaskAPI().then((res) => console.log(res.data.result));
+			this.initialTodoList();
+		},
+	},
+});
